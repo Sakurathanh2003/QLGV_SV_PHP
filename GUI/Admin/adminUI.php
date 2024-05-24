a08ab038a4195adc5eb122e7006e796a3408ad17
Vũ Thị Thanh <meimei@Air-cua-Vu.lan>
2024-05-24 22:52:41 +0700

    update AdminUI

--- GUI/Admin/adminUI.php
+++ GUI/Admin/adminUI.php
@@ -1,23 +1,83 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <?php
 include '../../Common/commonfunction.php';
 session_start();
 
 navigateIfNeed('admin');
-if (isset($_GET["logout"])) {
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     logout();
-}
+} 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="adminUI.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
 <body>
-    Admin
-    <form method="get">
-        <button name="logout">Logout</button>
-    </form>
+    <!--Left side bar-->
+    <div class="sidebar">
+        <div class="logo-content">
+            <div class="logo">
+            <i class='bx bxs-invader'></i>
+            <h3>Admin</h3>
+            </div>
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+
+        <ul class="list">
+            <li class="list-item active">
+                <a href="#">
+                    <i class='bx bxs-dashboard' ></i>
+                    <span class="links-name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li class="list-item">
+                <a href="#">
+                    <i class='bx bxs-dashboard' ></i>
+                    <span class="links-name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li class="list-item">
+                <a href="#">
+                    <i class='bx bxs-dashboard' ></i>
+                    <span class="links-name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li class="list-item">
+                <a href="#">
+                    <i class='bx bxs-dashboard' ></i>
+                    <span class="links-name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+        </ul>
+
+        <form action="" method="post" id="logoutForm">
+            <ul class="list-logout">
+                <li>
+                    <a href="#" onClick="login();">
+                        <i class='bx bx-log-out' ></i>
+                        <span class="links-name">Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </form>
+        <script>
+            function login() {
+                document.getElementById('logoutForm').submit();
+            }
+        </script>
+    </div>
+
+    <!--Content-->
+
+    <script src="adminUI.js"></script>
 </body>
 </html>
---
+++ GUI/Admin/logout.php
@@ -0,0 +1,5 @@
+<?php
+include '../../Common/commonfunction.php';
+
+logout();
+?>
